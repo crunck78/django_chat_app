e8de08a0cc9903fc7d8cd0065200667cb1a60fbc
Crunck78 <crunck78@googlemail.com>
2022-02-16 20:31:00 +0100

    create dabase object

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 
+from .models import Message, Chat
+
 # Create your views here.
+
+
 def index(request):
     if request.method == 'POST':
-        #https://stackoverflow.com/questions/5895588/django-multivaluedictkeyerror-error-how-do-i-deal-with-it
+        # https://stackoverflow.com/questions/5895588/django-multivaluedictkeyerror-error-how-do-i-deal-with-it
         #print("Received data " + request.POST.get('textmessage', ''))
-        print("Received data "  + request.POST['textmessage']) #most likely errors report if POST[keyname] does not match
+        # most likely errors report if POST[keyname] does not match
+        print("Received data " + request.POST['textmessage'])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(
+            text=request.POST['textmessage'],
+            chat=myChat,
+            author=request.user,
+            receiver=request.user
+        )
     return render(request, 'chat/index.html', {'username': 'Mihai'})
