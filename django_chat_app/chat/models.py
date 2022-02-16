80fccf51afb3b53ac4a00f64628a47ed683d4a94
Crunck78 <crunck78@googlemail.com>
2022-02-16 20:23:05 +0100

    makemigrations chat

--- django_chat_app/chat/models.py
+++ django_chat_app/chat/models.py
@@ -1,17 +1,31 @@
 from datetime import date, datetime
 from email.policy import default
+from xml.parsers.expat import model
 from django.db import models
-from django.db.models.fields import DateField
 from datetime import date
 from django.conf import settings
 
 from django.dispatch import receiver
 
 # Create your models here.
+
+
+class Chat(models.Model):
+    created_at = models.DateField(default=date.today)
+
+
 class Message(models.Model):
     text = models.CharField(max_length=500)
-    created_at = DateField(default=date.today)
+    created_at = models.DateField(default=date.today)
     # chat = Chat Klasse verknüfen
+    chat = models.ForeignKey(
+        Chat,
+        on_delete=models.CASCADE,
+        related_name='chat_message_set',
+        default=None,
+        blank=True,
+        null=True
+    )
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
