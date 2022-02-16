3e6a4667937ab722b74b81927d28c47ee64b9fd6
Crunck78 <crunck78@googlemail.com>
2022-02-16 19:52:23 +0100

    add post request handler

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        #https://stackoverflow.com/questions/5895588/django-multivaluedictkeyerror-error-how-do-i-deal-with-it
+        #print("Received data " + request.POST.get('textmessage', ''))
+        print("Received data "  + request.POST['textmessage']) #most likely errors report if POST[keyname] does not match
     return render(request, 'chat/index.html', {'username': 'Mihai'})
