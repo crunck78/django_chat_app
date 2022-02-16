5a38437d480556c125a77fcc1af21cb3b57df277
Crunck78 <crunck78@googlemail.com>
2022-02-16 13:24:35 +0100

    include material design, create Message Model

--- django_chat_app/chat/models.py
+++ django_chat_app/chat/models.py
@@ -1,3 +1,24 @@
+from datetime import date, datetime
+from email.policy import default
 from django.db import models
+from django.db.models.fields import DateField
+from datetime import date
+from django.conf import settings
+
+from django.dispatch import receiver
 
 # Create your models here.
+class Message(models.Model):
+    text = models.CharField(max_length=500)
+    created_at = DateField(default=date.today)
+    # chat = Chat Klasse verknüfen
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='author_message_set'
+    )
+    receiver = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='receiver_message_set'
+    )
