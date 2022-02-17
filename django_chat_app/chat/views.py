2ae9a42f7940972384f39d34fa18cad983da5287
Crunck78 <crunck78@googlemail.com>
2022-02-17 14:44:35 +0100

    register included

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
+from django.contrib.auth.models import User
+
 from .models import Message, Chat
 
 # Create your views here.
@@ -27,15 +29,47 @@ def index(request):
 
 
 def login_chat(request):
+    # This can be None
+    # Sure case: request.GET.get('next') = /chat/ if we got redirected from chat to login because of @login_require decorator
+    # Unknown case: request.GET.get('next') = anything , should this be an issue?
     redirect = request.GET.get('next')
-    if request.method == 'POST':
+    if request.method == 'POST':  # Handle POST Request
+       # Authenticate User
         user = authenticate(
             username=request.POST.get('username'),
             password=request.POST.get('password')
         )
-        if user:
+        if user: # Handle Authenticate Successfully
+            # Login User
             login(request, user)
-            return HttpResponseRedirect(request.POST.get('redirect'))
-        else:
+             # Redirect to chat
+            if(redirect == '/chat/'): # For now we only need to redirect ro chat
+                return HttpResponseRedirect(request.POST.get('redirect'))
+            else:
+                return HttpResponseRedirect('/chat/')
+        else: # Handle invalid Credentials
             return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
-    return render(request, 'auth/login.html', {'redirect': redirect})
+    return render(request, 'auth/login.html', {'redirect': redirect}) # Handle non POST Requests
+
+
+def register_chat(request):
+    redirect = request.GET.get('next')
+    if request.method == 'POST':   # Handle POST REQUEST
+      
+        if request.POST.get('password') == request.POST.get('check_password'):  # Handle User Complete Registration
+            # Create user
+            user = User.objects.create_user(
+                username=request.POST.get('username'),
+                email=request.POST.get('email'),
+                password=request.POST.get('password')
+            )
+             # Login User
+            login(request, user)
+            # Redirect to chat
+            if(redirect == '/chat/'): # For now we only need to redirect to chat
+                return HttpResponseRedirect(request.POST.get('redirect'))
+            else:
+                return HttpResponseRedirect('/chat/')
+        else: # Handle Passwort Check Failed
+            return render(request, 'auth/register.html', {'passwordNotMatch': True, 'redirect': redirect})
+    return render(request, 'auth/register.html', {'redirect': redirect})
